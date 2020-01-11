b7a6e952a2ee39d5702a9503b6ae4781bc3a31fa
Mikhail Eliseev <mishaeliseev1983@gmail.com>
2020-01-11 18:04:45 +0500

    added Skill Activity

---
+++ app/src/main/java/com/melyseev/basketswoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.melyseev.basketswoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : BasicActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
