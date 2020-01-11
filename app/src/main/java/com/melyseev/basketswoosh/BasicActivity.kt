07926f1ead0045af648ac968be5853d58b4eae44
Mikhail Eliseev <mishaeliseev1983@gmail.com>
2020-01-11 17:22:44 +0500

    added Basic Activity

---
+++ app/src/main/java/com/melyseev/basketswoosh/BasicActivity.kt
@@ -0,0 +1,44 @@
+package com.melyseev.basketswoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BasicActivity : AppCompatActivity() {
+
+    var TAG="LifeCycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, " ${javaClass.simpleName}  onCreate" )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, " ${javaClass.simpleName}  onStart" )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, " ${javaClass.simpleName}  onPause" )
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, " ${javaClass.simpleName}  onRestart" )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, " ${javaClass.simpleName}  onResume" )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, " ${javaClass.simpleName}  onStop" )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, " ${javaClass.simpleName}  onDestroy" )
+    }
+}
--- app/src/main/java/com/melyseev/basketswoosh/LeagueActivity.kt
+++ app/src/main/java/com/melyseev/basketswoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.melyseev.basketswoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
